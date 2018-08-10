7155e4fe125c706f41babecd5fe1d6cba1e1182d
test－mac <team@testmacdeMac-mini.local>
2018-08-10 15:15:52 +0800

    实现基本版本的组件化组件B 分类

---
+++ B_Category/B_Category/CTMediator+B.h
@@ -0,0 +1,15 @@
+//
+//  CTMediator+B.h
+//  B_Category
+//
+//  Created by test－mac on 2018/8/10.
+//  Copyright © 2018年 FK. All rights reserved.
+//
+
+#import "CTMediator.h"
+
+@interface CTMediator (B)
+
+- (UIViewController *)B_viewControllerWithContentText:(NSString *)contentText;
+
+@end
